9996cc36db0b1500137ce5c32924b0ea0f420156
gholtzap <gholtzap@asu.edu>
2023-03-18 19:43:52 -0700

    update

--- test.py
+++ test.py
@@ -4,8 +4,15 @@ from treelib import Node, Tree
 
 # big boys
 trees = []
+child = 0
 
-# creates a parent node
+def count_child_nodes(tree, parent_id):
+    child_count = 0
+    for node_id in tree.nodes:
+        node = tree.get_node(node_id)
+        if node.bpointer == parent_id:
+            child_count += 1
+    return child_count
 
 def create_event(value):
     tree = Tree()
@@ -13,29 +20,32 @@ def create_event(value):
     trees.append(tree)
     return tree
 
-# creates a child node
 def add_child(value, p):
-    trees[p].create_node(value, len(trees[p])+100, parent=p)
-
+    x = count_child_nodes(trees[p], p)
+    trees[p].create_node(value, 100+x, parent=p)
+    
 def delete_parent(value):
   trees.pop(value)
-    
+
+def delete_child(value, id):
+    trees[value].remove_node(id+100)
 
 # driver 
+
+
 
 create_event("Get CA return offer") # ID of this event is 0
 create_event("made to be deleted haha")
 create_event("Calculate salary")
 
-
-
 add_child("Buy parking at Fulton garage",0)
 add_child("Cancel OLIV lease",0)
 add_child("See if I have to file taxes", 2)
 
+delete_child(0,1)
 
-#delete_parent(1)
-#delete_parent(1)
+
+#delete_child(2,"See if I have to file taxes")
 
 for tree in trees:
     print(trees.index(tree), end=" ")
