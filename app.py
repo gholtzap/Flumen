dd3db87895970153652fb2777b366bdaca4a190a
gholtzap <gholtzap@asu.edu>
2023-03-29 20:50:01 -0700

    code cleanup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, jsonify, render_template, request
 from tree_mod import trees, add_child, create_event_app, delete_parent, delete_child
 
 app = Flask(__name__)
--- tree_mod.py
+++ tree_mod.py
@@ -1,4 +1,4 @@
-from treelib import Node, Tree
+from treelib import Tree
 
 # https://treelib.readthedocs.io/en/latest/
 
